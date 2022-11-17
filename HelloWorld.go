39d1caa2c8280c09a9984f0d70d35dff29536fe9
Ian Chiang <i29686112@gmail.com>
2022-11-17 22:31:37 +0800

    init

---
+++ HelloWorld.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func getTempString() (string, int) {
+	return "hello world yo", 1
+}
+
+func main() {
+	var str string
+
+	str, num := getTempString()
+	fmt.Println(str)
+	fmt.Println(num)
+
+	test := 123
+
+	fmt.Print(test)
+	coolFunc := func() { fmt.Println("cool man") }
+	coolFunc()
+
+	fmt.Println(main2())
+
+	fmt.Println(main3())
+}
+
+func main2() [10]string {
+
+	var array [10]string
+
+	for i := 0; i < 10; i++ {
+		array[i] = "123=>" + strconv.Itoa(i)
+	}
+
+	return array
+
+}
+
+func main3() map[string]int {
+
+	good := make(map[string]int)
+
+	good["ian"] = 100
+	good["jj"] = 200
+	return good
+}
